7bf4dfdc33fe830e7d3dfe3a3f49209c421dad61
Adiwijaya Satria Nusantara <adihanyu@gmail.com>
2023-04-20 07:49:16 +0700

    update

---
+++ script/Header.js
@@ -0,0 +1,46 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+        <nav
+            class="navbar navbar-expand-lg navbar-dark w-100"
+            style="background-color: transparent; height: 60px; position: absolute"
+            >
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link ${this.getAttribute('active') == 'home' ? 'active' : ''}" href="./home.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link ${this.getAttribute('active') == 'rules' ? 'active' : ''}" href="#">Rules</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link ${this.getAttribute('active') == 'rundown' ? 'active' : ''}" href="#">Rundown</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link ${this.getAttribute('active') == 'register' ? 'active' : ''}" href="./register.html">Register Now</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        `;
+  }
+}
+
+customElements.define("main-header", Header);
